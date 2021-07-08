64f8dcd5d67569df533c32e59b6f2dfbdac4ec6f
Nevrard <ndangaevrard@gmail.com>
2021-07-08 00:20:48 -0400

    check if app is online module

--- public/db.js
+++ public/db.js
@@ -5,3 +5,11 @@ request.onupgradeneeded = function (event) {
   const db = event.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
+request.onsuccess = function (event) {
+    db = event.target.result;
+  
+    // check if app is online before reading from db
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
