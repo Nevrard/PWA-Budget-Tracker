cff764ae8cccd26a6424b898ef139e0f00d8dbbe
Nevrard <ndangaevrard@gmail.com>
2021-07-08 01:00:12 -0400

    checkdatabase function

--- public/db.js
+++ public/db.js
@@ -24,3 +24,29 @@ request.onsuccess = function (event) {
   
     store.add(record);
   }
+
+  function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
+  }
